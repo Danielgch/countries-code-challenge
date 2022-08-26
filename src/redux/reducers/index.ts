e92cdc3869977981ec0393b276c7c6302e79ee00
Daniel Giraldo <danielgiraldo@gmail.com>
2022-08-25 21:41:55 -0500

    reducers

---
+++ src/redux/reducers/index.ts
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+
+import countryReducer from "./CountryReducer";
+
+const rootReducer = () =>
+  combineReducers({
+    countryReducer
+  });
+
+export default rootReducer;
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+import { CountryReducerState } from "./CountryTypes";
+export * from "./CountryTypes";
+
+//global App state
+
+export type AppState = {
+  countryReducer: CountryReducerState;
+};
